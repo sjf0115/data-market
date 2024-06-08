b75bdb7c941a613142e71c7541e8670e10ca99ce
sjf0115 <1203745031@qq.com>
2024-06-08 17:42:04 +0800

    MOD: RbmBitmapToStringUDF

--- hive-market/src/main/java/com/data/market/udf/RbmBitmapToStringUDF.java
+++ hive-market/src/main/java/com/data/market/udf/RbmBitmapToStringUDF.java
@@ -1,14 +1,15 @@
 package com.data.market.udf;
 
 import com.data.market.market.function.Rbm64Bitmap;
+import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.primitive.BinaryObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
-import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorUtils;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.WritableBinaryObjectInspector;
+import org.apache.hadoop.io.BytesWritable;
 
 import java.io.IOException;
 
@@ -19,9 +20,9 @@ import java.io.IOException;
  * 公众号：大数据生态
  * 日期：2024/5/17 06:51
  */
+@Description(name = "rbm_bitmap_to_str", value = "_FUNC_(bitmap) - Returns the string of a bitmap, separate with commas")
 public class RbmBitmapToStringUDF extends GenericUDF {
     private static String functionName = "rbm_bitmap_to_str";
-    private transient BinaryObjectInspector inspector;
 
     public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
         // 参数个数校验
@@ -31,10 +32,9 @@ public class RbmBitmapToStringUDF extends GenericUDF {
 
         // 参数类型校验
         ObjectInspector arg = arguments[0];
-        if (!(arg instanceof BinaryObjectInspector)) {
+        if (!(arg instanceof WritableBinaryObjectInspector)) {
             throw new UDFArgumentException("Argument of '" + functionName + "' should be binary type");
         }
-        this.inspector = (BinaryObjectInspector) arg;
 
         // 返回值类型
         return PrimitiveObjectInspectorFactory.javaStringObjectInspector;
@@ -45,7 +45,8 @@ public class RbmBitmapToStringUDF extends GenericUDF {
             return null;
         }
 
-        byte[] bytes = PrimitiveObjectInspectorUtils.getBinary(deferredObjects[0].get(), this.inspector).getBytes();
+        BytesWritable bw0 = (BytesWritable)(deferredObjects[0].get());
+        byte[] bytes = bw0.getBytes();
 
         try {
             Rbm64Bitmap bitmap = Rbm64Bitmap.fromBytes(bytes);
